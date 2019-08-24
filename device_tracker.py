4375c1b1d5559377a7d45fdd4c91bfe757b8a294
aalavender <jsdzh@hotmail.com>
2019-08-25 00:48:46 +0800

    测试完成

---
+++ device_tracker.py
@@ -0,0 +1,173 @@
+"""
+Support for TP-Link routers.
+
+For more details about this platform, please refer to the documentation at
+[url]https://home-assistant.io/components/device_tracker.tplink/[/url]
+"""
+import base64
+from datetime import datetime
+import hashlib
+import logging
+import re
+import json
+
+from aiohttp.hdrs import (
+    ACCEPT, COOKIE, PRAGMA, REFERER, CONNECTION, KEEP_ALIVE, USER_AGENT,
+    CONTENT_TYPE, CACHE_CONTROL, ACCEPT_ENCODING, ACCEPT_LANGUAGE)
+import requests
+import voluptuous as vol
+
+from homeassistant.components.device_tracker import (
+    DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
+from homeassistant.const import (
+    CONF_HOST, CONF_PASSWORD, CONF_USERNAME, HTTP_HEADER_X_REQUESTED_WITH)
+import homeassistant.helpers.config_validation as cv
+
+_LOGGER = logging.getLogger(__name__)
+
+HTTP_HEADER_NO_CACHE = 'no-cache'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Required(CONF_USERNAME): cv.string
+})
+
+
+def get_scanner(hass, config):
+    """Validate the configuration and return a TP-Link scanner."""
+    scanner = TplinkDeviceScanner(config[DOMAIN])
+    if scanner.success_init:
+        return scanner
+
+    return None
+
+
+class TplinkDeviceScanner(DeviceScanner):
+    """This class queries the WDR6500 router."""
+
+    def __init__(self, config):
+        """Initialize the scanner."""
+        host = config[CONF_HOST]
+        username, password = config[CONF_USERNAME], config[CONF_PASSWORD]
+        self.host = host
+        self.username = username
+        self.password = password
+        self.stok = ''
+        self.success_init = self._update_info()
+
+    def scan_devices(self):
+        """Scan for new devices and return a list with found device IDs."""
+        self._update_info()
+        #        self._log_out()
+        return self.last_results
+
+    # pylint: disable=no-self-use
+    def get_device_name(self, device):
+        """Get the firmware doesn't save the name of the wireless device.
+
+        """
+        return None
+
+    def _get_auth_tokens(self):
+        """Retrieve auth tokens from the router."""
+        _LOGGER.info("Retrieving auth tokens...")
+
+        url_login = 'http://{}/'.format(self.host)
+        Y_passwd = '{}'.format(self.password)
+        En_passwd = Encrypt(passwd=Y_passwd).encrypt_passwd()
+        post_data = {'login': {'password': En_passwd}, 'method': 'do'}
+        get_Text = requests.post(url=url_login, json=post_data).text
+        get_data = json.loads(get_Text)
+        _LOGGER.info('login response:' + get_Text)
+        self.stok = get_data['stok']
+        _LOGGER.info(self.stok)
+        if (self.stok == ''):
+            return false
+        return True
+
+    def _update_info(self):
+        """Ensure the information from the TP-Link router is up to date.
+
+        Return boolean if scanning successful.
+        """
+        if (self.stok == ''):
+            self._get_auth_tokens()
+
+        result = []
+        _LOGGER.info("Loading wireless clients...")
+        online_host = {"hosts_info": {"table": "online_host"}, "method": "get"}
+        url_info = ('http://{}/stok={}/ds').format(self.host, self.stok)
+        host_json = requests.post(url=url_info, json=online_host).json()
+        try:
+            if host_json.get('error_code') == 0:
+                info = host_json['hosts_info']['online_host']
+                for i in list(range(len(info))):
+                    result.append(list(info[i].values())[0]['mac'])
+            else:
+                if host_json.get('errorcode') == 'timeout':
+                    _LOGGER.info("Token timed out. Relogging on next scan")
+                    self.stok = ''
+                    return False
+                _LOGGER.error(
+                    "An unknown error happened while fetching data")
+                return False
+        except ValueError:
+            _LOGGER.error("Router didn't respond with JSON. Check if credentials are correct")
+            return False
+
+        if result:
+            #            _LOGGER.info(result)
+            #_LOGGER.info("Loading done.")
+            self.last_results = [mac.replace("-", ":") for mac in result]
+            return True
+
+        return False
+
+    def _log_out(self):
+        _LOGGER.info("Logging out of router admin interface...")
+
+        log_option = {"system": {"logout": "null"}, "method": "do"}
+        url_logout = ('http://{}/stok={}/ds').format(self.host, self.stok)
+        logOut_code = requests.post(url=url_logout, json=log_option).json()
+        if logOut_code['error_code'] == 0:
+            self.stok = ''
+
+
+class Encrypt:
+    def __init__(self, passwd=None, flat=1):
+        self.passwd = passwd
+        self.Flat = flat
+
+    def encrypt_passwd(self):
+        a = "RDpbLfCPsJZ7fiv"
+        c = 'yLwVl0zKqws7LgKPRQ84Mdt708T1qQ3Ha7xv3H7NyU84p21BriUWBU43odz3iP4rBL3cD02KZciXTysVXiV8ngg6vL48rPJyAUw0HurW20xqxv9aYb4M9wK1Ae0wlro510qXeU07kV57fQMc8L6aLgMLwygtc0F10a0Dg70TOoouyFhdysuRMO51yY5ZlOZZLEal1h0t9YQW0Ko7oBwmCAHoic4HYbUyVeU3sfQ1xtXcPcf1aT303wAQhv66qzW'
+        b = self.passwd
+        e = ''
+        f, g, h, k, l = 187, 187, 187, 187, 187
+        n = 187
+        g = len(a)
+        h = len(b)
+        k = len(c)
+        if g > h:
+            f = g
+        else:
+            f = h
+        for p in list(range(0, f)):
+            n = l = 187
+            if p >= g:
+                n = ord(b[p])
+            else:
+                if p >= h:
+                    l = ord(a[p])
+                else:
+                    l = ord(a[p])
+                    n = ord(b[p])
+            e += c[(l ^ n) % k]
+        try:
+            if self.Flat == 0:
+                _LOGGER.info(e)
+            else:
+                return e
+        except NameError:
+            pass
---
+++ readme.md
@@ -0,0 +1,46 @@
+# 简介
+Device_Tracer https://www.home-assistant.io/components/device_tracker/ 主要的功能是判断人是否在家和追踪GPS设备的位置，这两个都是实现自动化触发的优秀条件。
+
+举个例子：工作日早上7：30~8:30之间，从“在家”变成“不在家”，说明取上班了，可以触发一些自动化，比如推送路况到手机。同样，GPS追踪的话，可以在下班时间段，离家2公里以内，室内温度高于28度，就打开空调。
+
+判断是否在家比较靠谱的就是通过路由器拉取在线的mac地址列表，然后与事先定义好的 known_devices.yaml进行比较，
+比较遗憾的是，tp-link没有通用device_tracker的官方插件，好在万能论坛有人实现了 https://bbs.hassbian.com/thread-4737-1-1.html
+
+由于ha升级较快，在0.96版本中已经需要稍作修改才能用，所以本人稍作修改，github了一把。
+
+# 安装
+放入 <config directory>/custom_components/ 目录
+
+# 配置
+**Example configuration.yaml:**
+```yaml
+device_tracker:
+  - platform: tplink_tracker
+    host: 192.168.2.1
+    username: admin
+    password: 你的密码 
+    interval_seconds: 12
+    consider_home: 60
+    new_device_defaults:
+      track_new_devices: false
+      hide_if_away: false
+```
+
+# 属性说明
+| 属性 | 说明 | 
+| :-------------: |:-------------:| 
+| host | 无线路由器的登陆地址 | 
+| username | 没啥好说的 |
+| password | 没啥好说的 | 
+| interval_seconds | 两次扫描在线设备的时间间隔（秒） | 
+| consider_home | 当设备不在线多长时间(秒)后，判断为离家 | 
+| track_new_devices | 在路由器上自动发现known_devices.yaml定义以外的设备，是否要追踪，这个最好false，否则家里的在线设备阿狗阿猫都上来了） | 
+| hide_if_away | 判断为离家后，是否隐藏，这个默认为false | 
+
+每次扫描完成后，如果发现known_devices.yaml文件中没有的设备，会自动添加到known_devices.yaml
+即便track_new_devices: false也不例外，只是track:属性变为false，不追踪。
+
+# 前台界面
+
+![avatar](https://github.com/aalavender/device_tracker/blob/master/1.PNG)
+
